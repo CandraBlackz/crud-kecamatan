98f4e67809104348ae756c1bf33be2fbdef5eda5
CandraBlackz <blackz.wd@gmail.com>
2021-04-11 00:18:15 +0700

    make assigment coloumn

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     use HasFactory;
+
+    /**
+     * guarded
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * getImageAttribute
+     *
+     * @param mixed $image
+     * @return void
+     */
+    public function getImageAttribute($image)
+    {
+        return asset('storage/photos/' . $image);
+    }
 }
