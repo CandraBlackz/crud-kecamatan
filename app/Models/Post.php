acfbde13c7d3b85dfd337e0b782d028b10e9f23a
CandraBlackz <blackz.wd@gmail.com>
2021-04-11 13:55:41 +0700

    add relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,6 +18,26 @@ class Post extends Model
     protected $guarded = [];
 
     /**
+     * category
+     *
+     * return @void
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * tags
+     *
+     * @return void
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    /**
      * getImageAttribute
      *
      * @param mixed $image
@@ -25,5 +46,16 @@ class Post extends Model
     public function getImageAttribute($image)
     {
         return asset('storage/posts/' . $image);
+    }
+
+    /**
+     * getCreatedAtAttribute
+     *
+     * @param mixed $date
+     * @return void
+     */
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon::parse($date)->format('d-M-Y');
     }
 }
